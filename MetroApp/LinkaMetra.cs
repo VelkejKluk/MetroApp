7e07af6ac436100b1b28cb148a518160356c350d
Josef Navrátil <josef.navratil@educhem.cz>
2022-04-25 09:06:05 +0200

    Přidejte soubory projektu.

---
+++ MetroApp/LinkaMetra.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroApp
+{
+    internal class LinkaMetra
+    {
+        private int _pocetStanic;
+        private List<string> _nazvyStanic; //názvy stanic - Florenc, Pankrác, Hlavní nádraží...
+        public LinkaMetra(List<string> _vkladaneStanice)
+        {
+            _nazvyStanic = _vkladaneStanice;
+
+        }
+        public void VypisStanice()
+        {
+            foreach(string stanice in _nazvyStanic)
+            {
+                Console.WriteLine(stanice);
+            }
+
+        }
+        public void VypisPrvni()
+        {
+            Console.WriteLine(_nazvyStanic[0]);
+        }
+        public int ZjistiCas(string prvniStanice, string druhaStanice)
+        {
+            int cas = 0;
+            
+            if(prvniStanice == druhaStanice)
+            {
+                Console.WriteLine("Stanice se shodují");
+                //return cas;
+                
+            }
+            else
+            {
+                if (_nazvyStanic.Contains(prvniStanice) && _nazvyStanic.Contains(druhaStanice))
+                {
+                    int indexPrvni = _nazvyStanic.IndexOf(prvniStanice);
+                    int indexDruhe = _nazvyStanic.IndexOf(druhaStanice);
+                    int rozdil = indexPrvni - indexDruhe;
+                    cas = rozdil * 2;
+                }   
+            }
+            return cas;
+
+        }
+
+    }
+}
---
+++ MetroApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using MetroApp;
+
+Console.WriteLine("Pražské metro");
+
+List<string> linkaA = new List<string> { "Nemocnice Motol", "Petřiny", "Nádraží Veleslavín", "Bořislavka", "Dejvická", "Hradčanská", "Malostranská", "Staroměstská", "Muzeum", "Můstek", "Náměstí Míru", "Jiřího Poděbrad", "Flora", "Želivského", "STRAŠNICKÁ", "Skalka", "Depo Hostivař" };
+
+LinkaMetra linkaMetroA = new LinkaMetra(linkaA);
+linkaMetroA.VypisPrvni();
+Console.WriteLine("-----------");
+
+linkaMetroA.VypisStanice();
+Console.WriteLine("-----------");
+
+int cas = linkaMetroA.ZjistiCas("Boříslavka", "Nemocnice Motol");
+Console.WriteLine(cas);
